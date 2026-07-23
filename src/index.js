46c9521a51f813481d2353ebc9786c76b0837372
Haroon <mrharoonsalmankhan@gmail.com>
2026-07-23 14:06:46 +0500

    Added Service Worker for PWA

--- src/index.js
+++ src/index.js
@@ -15,3 +15,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => console.log('Service Worker Registered!', reg))
+      .catch((err) => console.log('Service Worker Error', err));
+  });
+}
